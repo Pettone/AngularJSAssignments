20f7c974e4bb5e058398fd4d68a7d8ca90351271
Pettone <peterleo2108@gmail.com>
2020-11-05 17:17:26 -0400

    5/11/2020 Assignments III

--- app.js
+++ app.js
@@ -4,34 +4,59 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController',NarrowItDownController)
 .service('MenuSearchService',MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItemsDirective);
+
+var result = [];
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
     var Narrow = this;
 
     Narrow.searchItem = "";
+    Narrow.array = [];
 
     Narrow.wantItem = function(){
-        var promise = MenuSearchService.getMatchedMenuItems();
+        var found = MenuSearchService.getMatchedMenuItems();
 
-        promise.then(function(response){
-            var result = [];
-            var foundItems = response.data;
+        found.then(function(response){
+            var items = [];
+            items = response.data.menu_items;
+            console.log(foundItems);
 
-            for (var i = 0; i < foundItems.length; i++) {
-                var name = foundItems[i].description;
+            for (var i = 0; i < items.length; i++) {
+                var name = items[i].description;
+            
                 if (name.toLowerCase().indexOf(Narrow.searchItem) !== -1) {
-                    result.push(foundItems[i]);
+                    result.push(items[i]);
                 }
             }
+            Narrow.array = result;
         })
         .catch(function (error) {
             console.log(error);
         });
-
-
     }
+
+    Narrow.removeItem = function(index){
+        MenuSearchService.removeItem(index);
+    }
+}
+
+function FoundItemsDirective(){
+    var ddo ={
+        templateUrl: 'foundItems.html',
+        scope: {
+            items: '<',
+            badRemove: '=',
+            onRemove: '&'
+        },
+        controller: NarrowItDownController,
+        controllerAs: 'Narrow',
+        bindToController: true
+    };
+
+    return ddo;
 }
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
@@ -46,7 +71,11 @@ function MenuSearchService($http, ApiBasePath){
       
           return response;
         };
+
+    service.removeItem = function (itemIndex){
+        result.splice(itemIndex,1);
     }
+}
 
 
 })();
